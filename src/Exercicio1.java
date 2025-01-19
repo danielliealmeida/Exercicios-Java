0521efb3f3090d9eba09514fc26e054d59e4c485
danielliealmeida <daniellieadm@gmail.com>
2025-01-19 14:08:19 -0300

    Initial commit

---
+++ src/Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        // Laço de repetição que vai de 1 até 20
+        for (int i = 1; i <= 20; i++) {
+            // Exibe a mensagem a cada iteração
+            System.out.println("Eu gosto de estudar Algoritmos!");
+        }
+    }
+}
